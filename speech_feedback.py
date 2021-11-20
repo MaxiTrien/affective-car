03f51d915fb07bb3fab955ca63a683fc96a7258d
MaxiTrien <51158324+MaxiTrien@users.noreply.github.com>
2021-11-20 16:43:32 +0100

    Add emotions and there repose from the tts system

--- speech_feedback.py
+++ speech_feedback.py
@@ -1,8 +1,18 @@
 import os 
+import time 
+import dataclasses
 
 from gtts import gTTS
 from playsound import playsound
+from dataclasses import dataclass
 
+
+@dataclass
+class FeedbackSentence:
+    angry: str = 'I can understand what you are feeling right now. Do you want to listen to some relaxing music?'
+    happy: str = 'You can relax and lean back. I will bring you to your destination safely.'
+    sad: str = 'I can understand what you are feeling right now. Do you want to listen to some music?'
+    
 
 def feedback(text):
     tts = gTTS(text=text, lang='en')
@@ -11,6 +21,11 @@ def feedback(text):
     tts.save(filename)
     playsound(filename)
     os.remove(filename)
+    
+    time.sleep(10)
+    
+    
+    
 
 
 
